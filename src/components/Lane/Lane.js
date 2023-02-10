9593ced811876a8f31f2ba42ac6813cfd885c0ce
Santosh Narayanan <santosh33@gmail.com>
2023-02-10 02:37:20 +0100

    Init Project with components

---
+++ src/components/Lane/Lane.js
@@ -0,0 +1,11 @@
+import './Lane.css';
+
+const Lane = ({title}) => {
+    return (
+        <div className="Lane-wrapper">
+            <h2>{title}</h2>
+        </div>
+    );
+};
+
+export default Lane;
