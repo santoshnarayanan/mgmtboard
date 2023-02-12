d4452e0cc8bc80b4a77108a0393385b4fe785b2f
Santosh Narayanan <santosh33@gmail.com>
2023-02-12 13:29:41 +0100

    Fetching Data using MyServer Json

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,5 +1,6 @@
 import Lane from '../components/Lane/Lane';
 import './Board.css';
+import {useState, useEffect} from "react";
 
 const lanes = [
     {id:1, title:'To Do'},
@@ -9,10 +10,41 @@ const lanes = [
 ];
 
 const Board = () => {
+    const [loading, setLoading] = useState(false);
+    const [tasks, setTasks] = useState([]);
+    const [error, setError] = useState('');
+    //using MyJson Server create data
+    const myUrl = 'https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/task';
+
+    useEffect(()=>{
+       async function fetchData(){
+           try{
+               const tasks = await fetch (myUrl);
+               const result = await tasks.join();
+               if(result){
+                   setTasks(result);
+                   setLoading(false); //loading is complete
+               }
+           }catch (e) {
+               setLoading(false); //if error then stop loading
+               setError(e.message);
+           }
+        }
+        fetchData();
+    },[]);
+
+
     return (
         <div className='Board-wrapper'>
             {lanes.map((lane) =>(
-                <Lane key={lane.id} title={lane.title} />
+                //distribute tasks over Lane component
+                <Lane
+                    key={lane.id}
+                    title={lane.title}
+                    loading={loading}
+                    error = {error}
+                    tasks = {tasks.filter((task)=> task.lane === lane.id)}
+                />
             ))}
         </div>
     );
