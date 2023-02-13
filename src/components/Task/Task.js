83344849e9ce6a75347f2836660eac6920abcc1d
Santosh Narayanan <santosh33@gmail.com>
2023-02-13 01:20:00 +0100

    Styled-Component in React

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,14 +1,26 @@
-import './Task.css'
+import styled from "styled-components";
+
+const TaskWrapper = styled.div`
+    background-color:darkgray;
+    padding:20px;
+    border-radius:20px;
+    margin:0% 5% 5% 5%;
+
+    h3{
+        width: 100%;
+        margin:0;
+    }
+    `;
 
 const Task = ({ id,title, body, onDragStart }) => {
     return (
-        <div className="Task-wrapper"
+        <TaskWrapper className="Task-wrapper"
              draggable
              onDragStart={(e) => onDragStart(e,id)}
         >
             <h3>{title}</h3>
             <p>{body}</p>
-        </div>
+        </TaskWrapper>
     );
 };
 
